aa1ce674823182d173b543bdbe943b1fcd841de5
Himesh Siriwardana <himeshsiriwardana@Himeshs-MacBook-Air.local>
2020-01-23 21:13:10 -0800

    Finishing the vulnerability scanners

--- xss.py
+++ xss.py
@@ -3,20 +3,16 @@ import nmap
 import requests
 import whois
 import subprocess
-
-
-# parser = argparse.ArgumentParser()
-# parser.add_argument("url", help="check for vulnerabilities in the given url")
-# parser.add_argument("action", help="full, info-gather, xss, sql")
-# parser.add_argument("checkPassword", help="f")
-# args = parser.parse_args()
-# print(args.echo)
+from openvas_lib import VulnscanManager, VulnscanException
+from threading import Semaphore
+from functools import partial
 
 
 '''Web Vulnerability Scanning'''
 #whois lookup of the target
 def whois_lookup(url):
-    print("Starting whois lookup")
+    print("+++++++++++++++++++++Starting whois lookup++++++++++++++++++")
+    time.sleep(2)
     domain = whois.query(url)
     print('[+]name: ', domain.name)
     print('[+]registrar: ', domain.registrar)
@@ -27,6 +23,8 @@ def whois_lookup(url):
 
 #Gaining http header information
 def header_information(url):
+    print("++++++++++++++++++++Checking for security headers+++++++++++++++++++")
+    time.sleep(2)
     resp = requests.get(url)
     try:
         header = resp.headers['Strict-Transport-Security']
@@ -64,9 +62,10 @@ def header_information(url):
         print('[-] X-Content-Type-Options header not set')
 
 
-def net_vuln_scan(url):
+def web_vuln_scan(url):
     subprocess.run("clear")
-    print("Running web vulnerability scanner ")
+
+    print("++++++++++++++++++++Running web vulnerability scanner++++++++++++++++++ ")
     scan = subprocess.run(["nikto +host %s"%url], stdout=subprocess.PIPE, shell=True)
     output = scan.stdout
     if scan.returncode:
@@ -77,16 +76,12 @@ def net_vuln_scan(url):
             print(vuln, '\n')
         
 
-
-
-
-
 '''Network Vulnerability Scanning'''
 #Port scanning and fingerprinting the target
 #nmap --reason -n -Pn --packet-trace -g 80 -sO -p 6 <target ip>
 #nmap --reason -n -Pn --packet-trace -g 80 -sA -p 80 <target ip>
 def port_scan(target):
-    print("Portscanning has started")
+    print("+++++++++++++++++++++Portscanning has started++++++++++++++++++++++++++")
     nm = nmap.PortScanner()
     scan = nm.scan(target, '22-443', arguments='-sV --script=/usr/local/share/nmap/scripts/vulscan' )['scan']['192.168.0.126']['tcp']
     ports = scan.keys()
@@ -106,12 +101,28 @@ def port_scan(target):
             for vuln in vulns:
                 print(vuln, '\n')
 
+def net_vuln_scan(host, user, password, target):
+    print("++++++++++++++++++++++++Running the network vulnerability scanner++++++++++++++++")
+    sem = Semaphore(0)
+    manager = VulnscanManager(host,user, password)
+    manager.launch_scan(target, profile = "empty", callback_end = partial(lambda x: x.release(), sem), callback_progress = my_print_status)
 
+    sem.acquire()
+    print("finished")
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("target", help="check for vulnerabilities in the given url or IP address")
+    #parser.add_argument("action", help="full, info-gather, xss, sql")
+    #parser.add_argument("checkPassword", help="f")
+    args = parser.parse_args()
+    if"http" in args.target:
+        print("Starting web vulnerability scanning on ", args.target)
+        print("---------------------------------------------------")
+        print("----------------Gathering information------------------")
     
-
-
-
-net_vuln_scan('http://192.168.0.126')
+#web_vuln_scan(args.target)
 
 # def xss(url):
 
